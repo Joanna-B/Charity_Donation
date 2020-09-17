c3057e21af64c97560fc730807d037d0ce4605c9
Joanna <jk.bednarek@gmail.com>
2020-09-17 15:28:42 +0200

    Models

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+INSTITUTION_TYPE = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna"),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField
+    school_class = models.IntegerField(choices=INSTITUTION_TYPE, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = PhoneField()
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- charity_project/settings.py
+++ charity_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'charity_app'
+    'charity_app',
+    'phone_field',
 ]
 
 MIDDLEWARE = [
