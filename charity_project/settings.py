429c1bb64145d97390f3fbbbb1bf0ceeac2b22cf
Joanna <jk.bednarek@gmail.com>
2020-09-17 13:54:44 +0200

    Templates

--- charity_project/settings.py
+++ charity_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +86,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 try:
--- charity_project/urls.py
+++ charity_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from charity_app.views import LandingPage, AddDonation, Login, Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as_view(), name="landing-page"),
+    path('add_donation/', AddDonation.as_view(), name="add-donation"),
+    path('login/', Login.as_view(), name="login"),
+    path('register/', Register.as_view(), name="login"),
 ]
